88d5c71721c5746d0b6ec9b43c60edf4f24b129d
T5UN4M1 <adrienabts@gmail.com>
2020-06-27 06:01:14 +0200

    added websockets and changed backend slightly for websockets

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,15 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as morgan from 'morgan';
 import * as log4js from 'log4js';
+import * as WebSocket from 'ws';
+import * as http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+
 const port = 8080;
-const logger = log4js.getLogger('');
+const logger = log4js.getLogger('app');
+
 logger.level = "debug";
 
 const morganLogger = morgan("combined", {
@@ -13,13 +18,14 @@ const morganLogger = morgan("combined", {
         write: str => logger.info(str)
     }
 });
-
 app.use(cors());
 app.use(morganLogger);
 
-app.listen(port, () => logger.info(`server started at http://localhost:${port}`));
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(port, () => logger.info(`server started at http://localhost:${port}`));
 
 
-
-
+export {app};
 
